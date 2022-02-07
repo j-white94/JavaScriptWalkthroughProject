1b79b9f9867c3f526ffccb18557d7fef65778222
j-white94 <jordanjwhite14@gmail.com>
2022-02-07 15:12:07 +0000

    Add favicon, tidy up page and controls wrt user

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,10 +15,13 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
-    runGame("subtract");
-    runGame("multiply");
-    runGame("divide");
 
 });
 
@@ -27,6 +30,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
@@ -139,7 +145,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 function displayDivideQuestion(operand1, operand2) {
 
-    document.getElementById("operand1").textContent = (operand1 % operand2) = 0 ? operand1 : displayDivideQuestion();
+    document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "÷";
 
